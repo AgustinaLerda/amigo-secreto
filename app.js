85362b832f95f419b038bd3e4bbb7c76326e9d23
AgusL <lerda.m.agustina@gmail.com>
2025-08-04 17:50:06 -0300

    Agregué la funcionalidad sortearAmigo()

--- app.js
+++ app.js
@@ -17,22 +17,25 @@ function validarEntrada(entrada){
 */ 
 function mostrarAmigos(){
 
-    
     let listaAmigosHTML= document.getElementById("listaAmigos");
     listaAmigosHTML.innerHTML="";
+    mostrarElemento("listaAmigos");
+
+
     //Para cada nombre de la lista de amigos
     for(let i = 0; i<amigos.length; i++){
         //Creo el li
         let nuevoAmigo= document.createElement("li");
-        //Seteo el nombre
+        //Cambio el nombre
         nuevoAmigo.textContent=amigos[i];
-        //seteo la clase
-        nuevoAmigo.className="nuevo-amigo";
     
-        //Agrego a la etiqueta 
+        //Agrego el nuevoAmigo al elemento ul 
         listaAmigosHTML.appendChild(nuevoAmigo);
     }
 
+     if( amigos.length != 0){
+        ocultarElemento("resultado");
+    }
 
 
 }
@@ -59,6 +62,45 @@ function agregarAmigo(){
     }
 }
 
+function indiceAleatorio(tamanioArreglo) {
+    //Genero un numero aleatorio que esté entre 0 
+    //y el maximo indice de la lista 
+    return Math.floor(Math.random()* tamanioArreglo) ;
+} 
 
+
+/* Función sortearAmigo():
+*       - Verifica que la lista de amigos no esté vacía
+*       - Calculo el indice aleatorio para buscar en la lista amigos
+*       - Oculto la lista de amigos que se muestra a medida que se cargan
+*       - Cargo el amigo en la etiqueta de id "resultado"
+*       - Muestro el elemento de id "reusltado"
+ */
+function sortearAmigo(){
+    if(amigos.length != 0){
+        let nombreAmigo=amigos[indiceAleatorio(amigos.length)];
+
+        ocultarElemento("listaAmigos");
+        document.getElementById("resultado").innerHTML =`El amigo secreto sorteado es: ${nombreAmigo}`;
+
+        mostrarElemento("resultado");
+    }
+    else{
+        alert("No ha ingresado ningún amigo.")
+        return;
+    }
+}
+
+
+/*Como indica su nombre muestran o ocultan un elemento de HTML 
+* según su id. Las cree para que sea mas facil de leer y entender
+* el código    
+*  */
+function mostrarElemento(id){
+    document.getElementById(id).style.display="block";
+}
+function ocultarElemento(id){
+    document.getElementById(id).style.display="none";
+}
 
 
