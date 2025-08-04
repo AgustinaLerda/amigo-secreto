c60f45e93f980b9e1b04ab79d6e4373bda8319b5
AgusL <lerda.m.agustina@gmail.com>
2025-08-04 15:43:42 -0300

    agrego funcion para guardar amigos

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Defino variables
+let amigos=[];
+
+
+//Verifica que el nombre ingresado no sea una cadena vacía
+function validarEntrada(entrada){
+    if(entrada == ""){
+        return false;
+    }
+}
+/* Funcion agregar amigo:
+*       - Agrega el nombre a la lista amigos
+*       - Muestra la cada nombre en la etiqueta ul de html
+*/ 
+function cargarAmigo(nombreAmigo){
+
+    amigos.push(nombreAmigo);
+
+    let listaAmigosHTML= document.getElementById("listaAmigos");
+    //Creo el li
+    let nuevoAmigo= document.createElement("li");
+    //Seteo el nombre
+    nuevoAmigo.textContent=nombreAmigo;
+    //seteo la clase
+    nuevoAmigo.className="nuevo-amigo";
+    
+    //Agrego a la etiqueta ul
+    listaAmigosHTML.appendChild(nuevoAmigo);
+
+}
+
+
+
+function agregarAmigo(){
+    //Obtengo el nombre ingresado al input de id:"amigo"
+    let nombre=  document.getElementById("amigo").value;
+
+    if(validarEntrada(nombre) == false){
+    
+        alert("No ha ingresado un nombre valido. Por favor, intentelo nuevamente.");
+    }
+    else{
+        cargarAmigo(nombre);
+
+        
+        document.getElementById("amigo").value="";
+    }
+
+
+}
+
+
